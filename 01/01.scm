c353530f4e9063642a26850d4eeb0dd9d7e8d0aa
falloutphil <phil@beadling.co.uk>
2021-12-06 09:00:01 +0000

    01 part 1

--- 01/01.scm
+++ 01/01.scm
@@ -5,6 +5,7 @@ exec guile -e '(@ (day01) main)' -s "$0" "$@"
 (define-module (day01)
   #:export (main)
   #:use-module (ice-9 rdelim) ;; read-line
+  #:use-module (srfi srfi-1) ;; count
   #:use-module (srfi srfi-42)) ;; list-ec
 
 (define (file->list filename transform)
@@ -12,10 +13,20 @@ exec guile -e '(@ (day01) main)' -s "$0" "$@"
     (lambda (p)
       (map transform (list-ec (:port line p read-line) line)))))
 
-;; change transform for function that actually returns the result!
+(define (make-count-increments)
+  (let ((last-value #f))
+    (lambda (str-depth-reading)
+      (let* ((depth-reading (string->number str-depth-reading))
+	     (result (if last-value
+			(- depth-reading last-value)
+			0)))
+	(set! last-value depth-reading)
+	result))))
+
 ;; change to macro with nicer syntax
-;; do you want to use map transform or right a recursive function?
+;; do you want to use map transform or write a recursive function - current implementation takes at least 2 passes?
 (define (main args)
-  (format #t
-	  "~%Result: ~a~%"
-	  (file->list "input.txt" string->number)))
+  (let ((counter (make-count-increments)))
+    (format #t
+	    "~%Result: ~a~%"
+	    (count positive? (file->list "input.txt" counter)))))
