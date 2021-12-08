72e891620cf2872812f8a8c137653c6a175872e3
falloutphil <phil@beadling.co.uk>
2021-12-08 22:41:32 +0000

    random ideas for storing state

--- 04/04.scm
+++ 04/04.scm
@@ -10,8 +10,29 @@ exec guile -e '(@ (day04) main)' -s "$0" "$@"
   #:use-module (srfi srfi-26) ;; cut
   #:use-module (srfi srfi-42)) ;; list-ec
 
-;; We don't convert to a bitvector here because list->array
-;; only handles lists of lists not lists of bitvectors.
+(define (make-bingo-number n)
+  (let ((called #f)
+	(value n))
+    (define (called?)
+      called)
+    (define (!called)
+      (set! called #t))
+    (define (get-value)
+      value)
+
+    (lambda args
+      (apply
+        (case (car args)
+          ((called?) called?)
+	  ((!called) !called)
+	  ((get-value) get-value)
+          (else (error "Invalid method!")))
+        (cdr args)))))
+
+
+(define (string->bingo-number s)
+  (make-bingo-number (string->number s)))
+
 (define (parse-input filename)
   "Read filename, first line to a list.
    Then place the rest into a 3D array."
@@ -21,7 +42,7 @@ exec guile -e '(@ (day04) main)' -s "$0" "$@"
 	     (numbers (map string->number (string-split (car lines) #\,)))
 	     (boards-str (delete '() (map (compose (cut delete "" <>) (cut string-split <> #\ ))
              				  (cdr lines))))
-	     (flat-boards ((compose (cut list->array 1 <>) (cut map string->number <>) concatenate) boards-str)))
+	     (flat-boards ((compose (cut list->array 1 <>) (cut map string->bingo-number <>) concatenate) boards-str)))
 	;(format #t "~%numbers: ~a~%" numbers)
 	;(format #t "~%boards-str: ~a~%" boards-str)
 	(values
@@ -31,5 +52,8 @@ exec guile -e '(@ (day04) main)' -s "$0" "$@"
 (define (main args)
   (let-values (((numbers boards) (parse-input "test_input.txt")))
     (format #t "~%numbers: ~a~%" numbers)
-    (format #t "~%first board: ~a~%" (array-cell-ref boards 0))))
+    ;(format #t "~%first board: ~a~%" (array-cell-ref boards 0))
+    (format #t "~%array values: ")
+    (array-for-each (lambda (i) (format #t "(~a ~a) " (i 'get-value) (i 'called?))) boards)
+    (format #t "~%test element: ~a~%" ((array-ref boards 0 0 0) 'get-value))))
     
