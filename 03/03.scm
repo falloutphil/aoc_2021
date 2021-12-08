18dbbbcba9f96b8e5dba7493512d321e618b9090
Phil.Beadling <phil.beadling@quantile.com>
2021-12-08 15:46:14 +0000

    more tidying

--- 03/03.scm
+++ 03/03.scm
@@ -53,34 +53,36 @@ exec guile -e '(@ (day03) main)' -s "$0" "$@"
 ;; Test input data from the example in the question.
 (define test-report
   #2((#f #f #\1 #f #f)
-    (#\1 #\1 #\1 #\1 #f)
-    (#\1 #f #\1 #\1 #f)
-    (#\1 #f #\1 #\1 #\1)
-    (#\1 #f #\1 #f #\1)
-    (#f #\1 #\1 #\1 #\1)
-    (#f #f #\1 #\1 #\1)
-    (#\1 #\1 #\1 #f #f)
-    (#\1 #f #f #f #f)
-    (#\1 #\1 #f #f #\1)
-    (#f #f #f #\1 #f)
-    (#f #\1 #f #\1 #f)))
+     (#\1 #\1 #\1 #\1 #f)
+     (#\1 #f #\1 #\1 #f)
+     (#\1 #f #\1 #\1 #\1)
+     (#\1 #f #\1 #f #\1)
+     (#f #\1 #\1 #\1 #\1)
+     (#f #f #\1 #\1 #\1)
+     (#\1 #\1 #\1 #f #f)
+     (#\1 #f #f #f #f)
+     (#\1 #\1 #f #f #\1)
+     (#f #f #f #\1 #f)
+     (#f #\1 #f #\1 #f)))
 
-(define (rating-filter bitarray n cmp)
+(define (rating-filter bitarray cmp)
   "Slice column and determine dominant bit in slice.
    Work out indices with the dominant bit (using cmp to compare).
    Create a new array with row numbers matching the indices.
    Repeat until one result left."
-  (let* ((lh-slice (array->list
-                    (make-shared-array bitarray
-                                       (lambda (i) (list i n)) ;; take slice holding column constant as n
-                                       `(0 ,(- (array-length bitarray) 1))))) ;; bound by length of array
-         (bv-slice (list->bitvector lh-slice)) ;; convert to bitvector
-         (dominant-bit (if (cmp (bitvector-count bv-slice) (/ (bitvector-length bv-slice) 2)) #\1 #f)) ;; count 1s and 0s - decide which is dominant
-         (indices (all-indices lh-slice dominant-bit)) ;; get indices
-         (new-array (list->array 2 (map (compose array->list (cut array-cell-ref bitarray <>)) indices)))) ;; contains only indices
-    (if (> (array-length new-array) 1)
-        (rating-filter new-array (+ n 1) cmp)
-        ((compose binary->decimal bitlist->list vector->list) (array-cell-ref new-array 0)))))   
+  (let loop ((bitarray bitarray)
+             (n 0))
+    (let* ((lh-slice (array->list
+                      (make-shared-array bitarray
+                                         (lambda (i) (list i n)) ;; take slice holding column constant as n
+                                         `(0 ,(- (array-length bitarray) 1))))) ;; bound by length of array
+           (bv-slice (list->bitvector lh-slice)) ;; convert to bitvector
+           (dominant-bit (if (cmp (bitvector-count bv-slice) (/ (bitvector-length bv-slice) 2)) #\1 #f)) ;; count 1s and 0s - decide which is dominant
+           (indices (all-indices lh-slice dominant-bit)) ;; get indices
+           (new-array (list->array 2 (map (compose array->list (cut array-cell-ref bitarray <>)) indices)))) ;; contains only indices
+      (if (> (array-length new-array) 1)
+          (loop new-array (+ n 1))
+          ((compose binary->decimal bitlist->list vector->list) (array-cell-ref new-array 0))))))
 
 
 ;; Using bitvectors turned out to be a pain because
@@ -88,16 +90,16 @@ exec guile -e '(@ (day03) main)' -s "$0" "$@"
 ;; Also bitvectors don't have a simple conversion to and from
 ;; binary numbers.
 (define (main args)
-  (let* ((one-false-array (list->array 2 (file->bitlist "input.txt"))) ; get the input convert to 2D array of 1s and #fs
-         (t-one-false-array (transpose-array one-false-array 1 0)) 
-         (t-bvec (map list->bitvector (array->list t-one-false-array))) ; convert transposed columns to list of bitvectors
-         ;; the question doesn't specify output when each bit is equally as common so ignore that case!
-         (gamma-list (map (compose (cut > <> 500) bitvector-count) t-bvec)) ; count the 1s for transposed column return #t for each >500
-         (gamma (binary->decimal (bitlist->list gamma-list))) ; convert the list into integers then decimal
-         (epsilon (logxor gamma #b111111111111))) ; flip the bits to get epsilon easily
-    (format #t "~%Part One: ~a~%" (* gamma epsilon)))
-  
-  (let* ((one-false-array (list->array 2 (file->bitlist "input.txt"))) ; get the input convert to 2D array of 1s and #fs
-         (oxygen (rating-filter one-false-array 0 >=))
-         (co2 (rating-filter one-false-array 0 <)))
-    (format #t "~%Part Two: ~a~%" (* oxygen co2))))
+  (let ((one-false-array (list->array 2 (file->bitlist "input.txt")))) ; get the input convert to 2D array of 1s and #fs)
+    ;; Part 1
+    (let* ((t-one-false-array (transpose-array one-false-array 1 0))
+           (t-bvec (map list->bitvector (array->list t-one-false-array))) ; convert transposed columns to list of bitvectors
+           ;; the question doesn't specify output when each bit is equally as common so ignore that case!
+           (gamma-list (map (compose (cut > <> 500) bitvector-count) t-bvec)) ; count the 1s for transposed column return #t for each >500
+           (gamma (binary->decimal (bitlist->list gamma-list))) ; convert the list into integers then decimal
+           (epsilon (logxor gamma #b111111111111))) ; flip the bits to get epsilon easily
+      (format #t "~%Part One: ~a~%" (* gamma epsilon)))
+    ;; Part 2
+    (let ((oxygen (rating-filter one-false-array >=))
+          (co2 (rating-filter one-false-array <)))
+    (format #t "~%Part Two: ~a~%" (* oxygen co2)))))
