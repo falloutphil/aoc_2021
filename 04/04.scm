2c4dde470a7c86f38800d5260dcb2feffb9903c5
Phil.Beadling <phil.beadling@quantile.com>
2021-12-09 18:42:14 +0000

    remove col/row duplication

--- 04/04.scm
+++ 04/04.scm
@@ -13,7 +13,7 @@ exec guile -e '(@ (day04) main)' -s "$0" "$@"
 (define (make-bingo-number n)
   "Stateful number object that knows if it's been called."
   (let ((called #f)
-	(value n))
+        (value n))
     (define (called?)
       called)
     (define (!called)
@@ -25,8 +25,8 @@ exec guile -e '(@ (day04) main)' -s "$0" "$@"
       (apply
         (case (car args)
           ((called?) called?)
-	  ((!called) !called)
-	  ((get-value) get-value)
+          ((!called) !called)
+          ((get-value) get-value)
           (else (error "Invalid method!")))
         (cdr args)))))
 
@@ -40,14 +40,35 @@ exec guile -e '(@ (day04) main)' -s "$0" "$@"
   (call-with-input-file filename
     (lambda (p)
       (let* ((lines (list-ec (:port line p read-line) line))
-	     (numbers (map string->number (string-split (car lines) #\,)))
-	     (boards-str (delete '() (map (compose (cut delete "" <>) (cut string-split <> #\ ))
-             				  (cdr lines))))
-	     (flat-boards ((compose (cut list->array 1 <>) (cut map string->bingo-number <>) concatenate) boards-str)))
-	(values
-	 numbers
-	 (make-shared-array flat-boards (lambda (i j k) (list (+ (* i 25) (* j 5) k))) 100 5 5)))))) ;; HINT: grep '^$' input.txt | wc -l 
+             (numbers (map string->number (string-split (car lines) #\,)))
+             (boards-str (delete '() (map (compose (cut delete "" <>) (cut string-split <> #\ ))
+                                          (cdr lines))))
+             (flat-boards ((compose (cut list->array 1 <>) (cut map string->bingo-number <>) concatenate) boards-str)))
+        (values
+         numbers
+         (make-shared-array flat-boards (lambda (i j k) (list (+ (* i 25) (* j 5) k))) 100 5 5)))))) ;; HINT: grep '^$' input.txt | wc -l 
 
+
+(define (check-line break board-idx test-board direction called-number)
+  "Check bingo lines for a specific board in a specific direction for one called number."
+  (let line-loop ((x 0))
+    (define (make-slicer direction)
+      "Slice a column or row at position i."
+      (cond
+        ((eq? direction 'col) (lambda (i) (list i x)))
+        ((eq? direction 'row) (lambda (i) (list x i)))
+        (else (error "Invalid direction!"))))
+    (let ((slice (make-shared-array test-board
+                                    (make-slicer direction) ;; take slice holding row/col constant
+                                    '(0 4)))
+          (bingo #t))
+      (array-for-each (lambda (i)
+                        ;(format #t "~%~a slice: (~a ~a) " direction (i 'get-value) (i 'called?))
+                        (set! bingo (and bingo (i 'called?))))
+                      slice)
+      ;(format #t "~%bingo: ~a" bingo)
+      (when bingo (break (list board-idx called-number))))
+    (when (< x 4) (line-loop (+ x 1)))))
 
 (define (bingo? break boards num)
   "Called after each number is drawn."
@@ -56,30 +77,9 @@ exec guile -e '(@ (day04) main)' -s "$0" "$@"
   "Exit via continuation if any col or row is all called." 
   (let board-loop ((n 0))
     (let ((test-board (array-cell-ref boards n)))
-      ;; col-loop and row-loop should be refactored into one!
-      (let col-loop ((x 0))
-	(let ((slice (make-shared-array test-board
-			   (lambda (i) (list i x)) ;; take slice holding column constant as x
-			   '(0 4)))
-	      (bingo #t))
-	  (array-for-each (lambda (i) (set! bingo (and bingo (i 'called?))))
-				  ;(format #t "~%col slice: (~a ~a) " (i 'get-value) (i 'called?)))
-			  slice)
-	  ;;(format #t "~%bingo: ~a" bingo)
-	  (when bingo (break (list n num))))
-	(when (< x 4) (col-loop (+ x 1))))
-      (let row-loop ((x 0))
-	(let ((slice (make-shared-array test-board
-			   (lambda (i) (list x i)) ;; take slice holding row constant as x
-			   '(0 4)))
-	      (bingo #t))
-	  (array-for-each (lambda (i) (set! bingo (and bingo (i 'called?))))
-				  ;(format #t "~%row slice: (~a ~a) " (i 'get-value) (i 'called?)))
-			  slice)
-	  ;;(format #t "~%bingo: ~a" bingo)
-	  (when bingo (break (list n num))))
-	(when (< x 4) (row-loop (+ x 1))))
-    (when (< n 99) (board-loop (+ n 1)))))) ;; don't forget to change me if the input file changes!
+      (check-line break n test-board 'col num)
+      (check-line break n test-board 'row num)
+     (when (< n 99) (board-loop (+ n 1)))))) ;; don't forget to change me if the input file changes!
 
 
 (define (call-and-check break arr num)
