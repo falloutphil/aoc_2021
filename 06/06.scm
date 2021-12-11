1384921a9ed406704247db597cb4f9b1dc19a968
falloutphil <phil@beadling.co.uk>
2021-12-11 16:20:45 +0000

    day 6 part 1

--- 06/06.scm
+++ 06/06.scm
@@ -9,10 +9,26 @@ exec guile -e '(@ (day06) main)' -s "$0" "$@"
   #:use-module (srfi srfi-26)) ;; cut
 
 (define (get-input filename)
-  "Take first line of filename as input."
-  (call-with-input-file filename read-line))
-  
-  
+  "Take first line of filename as input.
+   Convert to list of numbers."
+  (map string->number
+       (string-split (call-with-input-file filename read-line)
+		     #\,)))
+
+(define (fish-evolve fish)
+  "A 0 becomes a 6, and adds a new 8."
+  "Every other number is decreased by 1."
+  (if (eqv? fish 0)
+      '(6 8)
+      (list (- fish 1))))
+
+(define (school-evolve fishes day)
+  "Tick forwards one day."
+  ;(format #t "~%~%fish: ~a length: ~a~%" fishes (length fishes))
+  (if (> day 80)
+      (length fishes)
+      (school-evolve (concatenate (map fish-evolve fishes)) (+ day 1))))
+
 (define (main args)
-  (let ((input (get-input "test_input.txt")))
-    (format #t "~%~%input: ~a~%" input)))
+  (let ((fishes (get-input "input.txt")))
+    (format #t "~%~%part 1: ~a~%" (school-evolve fishes 1))))
