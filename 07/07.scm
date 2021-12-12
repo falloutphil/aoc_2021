e7e3e4193ad4d1cd803e7c00ff9bd6146e1c6278
falloutphil <phil@beadling.co.uk>
2021-12-12 11:46:08 +0000

    day 7 part 2

--- 07/07.scm
+++ 07/07.scm
@@ -15,26 +15,44 @@ Or as we have an even number of candidates we should be able to sort the list an
 (define-module (day07)
   #:export (main)
   #:use-module (ice-9 rdelim) ;; read-line
-  #:use-module (srfi srfi-1) ;; concatenate
-  #:use-module (srfi srfi-11) ;; let-values
+  #:use-module (srfi srfi-1) ;; reduce
+  #:use-module (srfi srfi-26) ;; cut
   #:use-module (srfi srfi-43)) ;; vector-fold
 
 (define (get-input filename)
   "Take first line of filename as input.
    Convert to list of numbers."
-  (list->vector (map string->number
+  (map string->number
        (string-split (call-with-input-file filename read-line)
-		     #\,))))
+		     #\,)))
+
+(define (sum-fuel-cost locs candidate)
+  "Part 2 cost of moving all locations to the candidate point."
+  (reduce + 0
+	  (map (lambda (loc)
+		 (let ((abs-dist (abs (- loc candidate))))
+		   (euclidean-quotient (* abs-dist (+ abs-dist 1)) 2)))
+	       locs)))
 
 (define (main args)
   ;; Let's use the mid-value of a sorted list of even elements method
-  (let* ((sorted-locs (sort (get-input "input.txt") <))
+  ;; Part 1
+  (let* ((sorted-locs (sort ((compose list->vector get-input) "input.txt") <))
 	 (high-mid (/ (vector-length sorted-locs) 2)) ;; ignore 0 indexing to get high-mid
 	 (median-pos (/ (+ (vector-ref sorted-locs (- high-mid 1)) ;; then decrement to get low-mid
 			   (vector-ref sorted-locs high-mid))
 			2)))
     (format #t "~%~%part 1: ~a~%"
-		 (vector-fold
-		  (lambda (_ count pos)
-		    (+ count (abs (- pos median-pos)))) ;; fuel cost is abs distance
-		  0 sorted-locs))))
+	    (vector-fold
+	     (lambda (_ count pos)
+	       (+ count (abs (- pos median-pos)))) ;; fuel cost is abs distance
+	     0 sorted-locs)))
+
+  ;; Part 2
+  ;; Ignoring the temptation to simplify this to mean for large positions
+  ;; Instead here's the mechanical approach
+  (let* ((locs (get-input "input.txt"))
+	 (candidate-positions (iota (apply max locs))))
+    (format #t "~%~%part 1: ~a~%"
+	    (apply min (map (cut sum-fuel-cost locs <>) candidate-positions)))))
+	 
