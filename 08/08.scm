e696e9c6f1aedd114158615589a84db40750ff47
falloutphil <phil@beadling.co.uk>
2021-12-12 21:41:25 +0000

    dodgy start to part 2

--- 08/08.scm
+++ 08/08.scm
@@ -61,11 +61,36 @@ g -> Take 0, remove 1 (c f), remove a, b, e
 (define (count-cipher-segments segments)
   (let ((cipher-lst (cadr segments)))
     (map string-length cipher-lst))) 
-	
+
+(define (first-pass digit-segments)
+  (let ((segments (string-length digit-segments)))
+    (case segments
+      ((2) `(1 . ,digit-segments))
+      ((4) `(4 . ,digit-segments))
+      ((3) `(7 . ,digit-segments))
+      ((7) `(8 . ,digit-segments))
+      (else `(#f . ,digit-segments)))))
+
+(define (find-a segments-assoc)
+  (format #t "~%segment-assoc: ~a" segments-assoc)
+  (lset-difference eqv?
+		   (string->list (assv-ref segments-assoc 7)) ;; MOST digits comes first in exp
+		   (string->list (assv-ref segments-assoc 1)))) 
+   
+  
+(define (determine-cryptograph one-line-segments)
+  (let ((digit-lst (car one-line-segments)))
+    (map first-pass digit-lst)))
+    
 (define (main args)
-  (let* ((di (file->digit-inputs "input.txt"))
+  (let* ((di (file->digit-inputs "test_input.txt"))
 	 (ciphertext-segment-counts (concatenate (map count-cipher-segments di))))
     ;;(format #t "~%~%Input: ~a~%" ciphertext-segment-counts)
+    ;; Part 1
     (format #t "~%~%Part 1: ~a~%" (count (lambda (n)
 					   (case n ((2 4 3 7) #t) (else #f)))
-					 ciphertext-segment-counts))))
+					 ciphertext-segment-counts))
+    ;; Part 2
+    (let* ((number-assocs (map determine-cryptograph di))
+	  (a-segments (map find-a number-assocs)))
+      (format #t "~%~%Part 2: ~a~%" a-segments))))
