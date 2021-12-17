9dc7b5d142b4b47bcf14ed69438a6224fd2b5d05
falloutphil <phil@beadling.co.uk>
2021-12-17 01:15:38 +0000

    tactical commit

--- 09/09.scm
+++ 09/09.scm
@@ -65,7 +65,7 @@ OTHERWISE
 #t c  #t
 #f #t #f
 
-#!
+!#
 
 (define-module (day09)
   #:export (main)
@@ -95,7 +95,8 @@ OTHERWISE
   "A naff way of resetting the array origin to (0 0)."
   (list->array 2 (array->list arr)))
 
-(define (make-sub-grid world)
+;; not convinced about origin-translation!
+(define (make-sub-grid world origin-translation)
   "Returns a function that takes a slice of the large array."
   (lambda (i-bounds j-bounds)
     ;; list is just a pass-through so we don't change
@@ -104,7 +105,7 @@ OTHERWISE
     ;; without coordinates.
     ;; The trick will be filtering out all the diagonals!
     ;;(format #t "~%i bounds: ~a j bounds: ~a" i-bounds j-bounds)
-    (zero-array-origin (make-shared-array world list i-bounds j-bounds))))
+    (origin-translation (make-shared-array world list i-bounds j-bounds))))
 
 
 ;; Arrays returned by make-sub-grid are regular/rectangular arrays,
@@ -143,20 +144,18 @@ OTHERWISE
 (define (low-point? world coords)
   "Are the coords a low-point on the world array?"
   (let* ((result #t)
-         (i (car coords)) ;; world coords
-         (j (cadr coords))
-         (grid-pair (adjacent-grid world i j)) ;; mask and subarray as pair
+         (grid-pair (adjacent-grid world coords zero-array-origin)) ;; mask and subarray as pair
          (sub (cdr grid-pair))
          (c-mask (car grid-pair))
          ;; this is the reason we have to 0-anchor our arrays which is expensive :-(
          (c (array-ref sub (centred-mask-i c-mask) (centred-mask-j c-mask)))) ;; value @ centre
-    (format #t "~%world coords: (~a, ~a)" i j)
-    (format #t "~%grid: ~a" (centred-mask-grid c-mask))
-    (format #t "~%i: ~a" (centred-mask-i c-mask))
-    (format #t "~%j: ~a"  (centred-mask-j c-mask))
-    (format #t "~%centre: ~a" c)
+    ;;(format #t "~%world coords: (~a, ~a)" i j)
+    ;;(format #t "~%grid: ~a" (centred-mask-grid c-mask))
+    ;;(format #t "~%i: ~a" (centred-mask-i c-mask))
+    ;;(format #t "~%j: ~a"  (centred-mask-j c-mask))
+    ;;(format #t "~%centre: ~a" c)
     (array-for-each (lambda (sub-v mask-v)
-                      (format #t "~%sub-v: ~a mask-v: ~a" sub-v mask-v)
+                      ;;(format #t "~%sub-v: ~a mask-v: ~a" sub-v mask-v)
                       (when mask-v (set! result (and result (< c sub-v))))) ;; the key line!
                     sub (centred-mask-grid c-mask))
     result))
@@ -176,12 +175,14 @@ OTHERWISE
 
 ;; Create the slice from the world and associates
 ;; it with a mask of points to check.
-(define (adjacent-grid world i j)
+(define (adjacent-grid world coords origin-translation)
   "Return grid centered on (i j)."
-  (let* ((dims (array-dimensions world))
+  (let* ((i (car coords))
+	 (j (cadr coords))
+	 (dims (array-dimensions world))
 	 (cols (1- (cadr dims)))
 	 (rows (1- (car dims)))
-	 (sub-grid (make-sub-grid world)))
+	 (sub-grid (make-sub-grid world origin-translation)))
     ;;(format #t "~%cols: ~a rows: ~a" cols rows)
     (cond ;; order is important!
      ;; top left
@@ -203,23 +204,72 @@ OTHERWISE
      ;; elsewhere, not on an edge
      (else `(,other . ,(sub-grid (list+-inc i) (list+-inc j)))))))
 
+(define (make-2d-coords arr)
+  "Create a set of coordinate pairs for the given array."
+  (let ((dims (array-dimensions arr)))
+    (concatenate
+     (map (lambda (col)
+	    (map (lambda (row) (list col row))
+		 (iota (cadr dims))))
+	  (iota (car dims))))))
+
+(define (make-upstream? centre)
+  (lambda (element mask-element coord)
+    (cond
+     ((eqv? element 9) #f)
+     ((< element centre) #f) ;; if the candidate is less than the centre, it must be downstream
+     ((not mask-element) #f) ;; ignore diagonals
+     (else (begin (format #t "~%coord: ~a" coord) coord)))))
+
+(define (process world low-point)
+  (format #t "~%low-point ~a" low-point)
+  (let* ((adjacent (adjacent-grid world low-point identity))
+	 (mask (car adjacent))
+	 (grid (cdr adjacent))
+	 (c (array-ref (zero-array-origin grid) (centred-mask-i mask) (centred-mask-j mask)))
+	 (upstream? (make-upstream? c))) ;; value @ centre)
+    (filter-map upstream?
+		(concatenate (array->list grid))
+		(concatenate (array->list (centred-mask-grid mask)))
+		(make-2d-coords grid))))
+
+(define (recurse-basins world low-points)
+  (cond
+   ((null? low-points) '())
+   (else (cons (recurse-basins world (process world (car low-points)))
+	       (recurse-basins world (cdr low-points))))))
 
 (define (main args)
-  (let* ((world (parse-input "input.txt"))
-         (dims (array-dimensions world))
-         (world-coord-pairs (concatenate (map (lambda (col)
-                                                (map (lambda (row) (list col row))
-                                                     (iota (cadr dims))))
-                                              (iota (car dims))))))
-    (format #t "~%world: ~a~%" world)
-    (format #t "~%world array dimensions: ~a~%" world-coord-pairs)
+  (let* ((world (parse-input "test_input.txt"))
+         (world-coord-pairs (make-2d-coords world)))
+    ;;(format #t "~%world: ~a~%" world)
+    ;;(format #t "~%world array dimensions: ~a~%" world-coord-pairs)
     (let ((low-points (filter (lambda (coords)
                                 (let ((result (low-point? world coords)))
-                                  (format #t "~%filter result: ~a~%" result)
+                                  ;;(format #t "~%filter result: ~a~%" result)
                                   result))
-                              world-coord-pairs)))
+			      world-coord-pairs)))
       (format #t "~%~%Part 1: ~a~%"
-              (fold (lambda (p sum)
-                      (+ sum 1 (array-ref world (car p) (cadr p))))
+	      (fold (lambda (p sum)
+		      (+ sum 1 (array-ref world (car p) (cadr p))))
                     0
-                    low-points)))))
+                    low-points))
+      (recurse-basins world low-points)
+      )))
+
+
+#!
+
+Get the filtered list of low-points.
+
+We're going to cons our way through it, relacing each low point with a set of coords.
+
+For each low point get the adjacent grid coords.
+
+Filter out 9s
+
+length of list left
+0 '()
+else (map fn new list)
+
+!#
