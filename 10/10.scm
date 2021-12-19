fb8a4ae7e99e709ff90fc7505a81082b77163687
falloutphil <phil@beadling.co.uk>
2021-12-19 12:02:22 +0000

    tactical save

--- 10/10.scm
+++ 10/10.scm
@@ -16,5 +16,36 @@ exec guile -e '(@ (day10) main)' -s "$0" "$@"
     (lambda (p)
       (map string->list (list-ec (:port line p read-line) line)))))
 
+
+;; filter for openners
+;; convert to closers and reverse
+;; should match filter for closers
+
+(define (open-bracket b-char)
+  (case b-char
+    ((#\( #\[ #\< #\{) #t)
+    (else #f)))
+
+(define (close-bracket b-char)
+  (case b-char
+    ((#\) #\] #\> #\}) #t)
+    (else #f)))
+
+(define (translate-bracket b-char)
+  (format #t "~%b-char: ~a" b-char)
+  (case b-char
+    ((#\() #\))
+    ((#\[) #\])
+    ((#\<) #\>)
+    ((#\{) #\})
+    (else (error "impossible!"))))
+    
+    
 (define (main args)
-  (format #t "~%file: ~a~%" (file->list "test_input.txt")))
+  (let* ((list-of-brackets (file->list "test_input.txt"))
+	 (open-list (map (cut filter open-bracket <>) list-of-brackets))
+	 (close-list (map (cut filter close-bracket <>) list-of-brackets)))
+    (format #t "~%file: ~a~%" (file->list "test_input.txt"))
+    (format #t "~%open: ~a~%" open-list)
+    (format #t "~%close: ~a~%" close-list)
+    (format #t "~%result1: ~a~%result2: ~a~%" (reverse (map (cut map translate-bracket <>) open-list)) close-list)))
