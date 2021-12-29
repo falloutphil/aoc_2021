40009fb7302e7ddcab875e5023b286418f21be51
falloutphil <phil@beadling.co.uk>
2021-12-29 23:13:21 +0000

    notes

--- 12/12.scm
+++ 12/12.scm
@@ -12,6 +12,32 @@ exec guile -e '(@ (day12) main)' -s "$0" "$@"
   #:use-module (srfi srfi-26) ;; cut
   #:use-module (srfi srfi-42)) ;; list-ec/eager comprehensions
 
+#!
+Construct a dictionary of nodes
+Where the values are a list of onwards nodes
+
+connect = like a defaultdict(list)
+
+Split each pair into the list
+(he DX)
+reverse the pair
+(DX he)
+
+Zip them together
+((he DX) (DX he))
+
+for he, DX
+  if DX != 'start'
+     connect[he].append(DX)
+
+for DX, he
+  if DX != 'start'
+     connect[DX].append(he)
+
+# can't go from end to something else
+del connect[end]
+
+!#
 
 (define (main args)
   (format #t "~%hello~%"))
