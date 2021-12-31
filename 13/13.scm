e1c00491797c43bbc2796fade650d26d5db56aee
falloutphil <phil@beadling.co.uk>
2021-12-31 09:53:55 +0000

    use syntax-parse to read input

--- 13/13.scm
+++ 13/13.scm
@@ -3,18 +3,32 @@
 exec guile -e '(@ (day13) main)' -s "$0" "$@"
 !#
 
+;; https://gitlab.com/guile-syntax-parse/guile-syntax-parse
+(add-to-load-path  "/home/phil/installs/guile-syntax-parse")
+
 (define-module (day13)
   #:export (main)
+  #:use-module (syntax parse)
   #:use-module (ice-9 rdelim) ;; read-line
   #:use-module (ice-9 match)
   #:use-module (ice-9 hash-table)
   #:use-module (srfi srfi-1) ;; last
-  #:use-module (srfi srfi-11) ;; let-values
   #:use-module (srfi srfi-26) ;; cut
   #:use-module (srfi srfi-42)) ;; list-ec/eager comprehensions
 
+
+;; Macro to split the result of file->list
+;; into a list of coords and a list of folds.
+(define parse-input
+  (syntax-parser
+   [(coords ...+ #f folds ...+)
+    #'((coords ...) (folds ...))])
+  )
+
 (define (file->list filename)
-  (let* ((lst (call-with-input-file filename
+  "Read input and split into list of 
+   coordinates and folds."
+  (let ((lst (call-with-input-file filename
 	       (λ (p)
 		 (list-ec (:port line p read-line)
 			  (cond
@@ -22,17 +36,15 @@ exec guile -e '(@ (day13) main)' -s "$0" "$@"
 			    (string-split line #\,))
 			   ((string-null? line) #f) ;; blank line
 			   ((string= line "fold along x=" 1 13 1 13)
-			    (last (string-split line #\=)))
+			    `(x ,(last (string-split line #\=))))
 			   ((string= line "fold along y=" 1 13 1 13)
-			    (last (string-split line #\=)))
-			   (else (error "bad input!")))))))
-	 (coords (take-while identity lst)) ;; up to blank line
-	 (folds (cdr (drop-while identity lst))))      
-    (values coords (first folds) (second folds))))
+			    `(y ,(last (string-split line #\=))))
+			   (else (error "bad input!"))))))))
+    (parse-input lst)))
+      
 
 
 (define (main args)
-  (let-values (((coords x-fold y-fold) (file->list "test_input.txt")))
+  (match-let (((coords folds) (file->list "input.txt")))
     (format #t "~%coords: ~a~%" coords)
-    (format #t "~%x-fold: ~a~%" x-fold)
-    (format #t "~%y-fold: ~a~%" y-fold)))
+    (format #t "~%folds: ~a~%" folds)))
