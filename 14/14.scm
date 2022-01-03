33440a597420f74078cb91934a9bb3b36bedcc8b
falloutphil <phil@beadling.co.uk>
2022-01-03 14:58:04 +0000

    file parser

--- 14/14.scm
+++ 14/14.scm
@@ -13,5 +13,14 @@ exec guile -e '(@ (day14) main)' -s "$0" "$@"
   #:use-module (srfi srfi-42)) ;; list-ec/eager comprehensions
 
 
+(define (file->inputs filename)
+  (call-with-input-file filename
+    (λ (p)
+      (values (read-line p)
+	      (cdr (list-ec (:port line p read-line) ;; cdr removes blank line
+			    (string-tokenize line char-set:upper-case)))))))
+
 (define (main args)
-  (format #t "~%Hello~%"))
+  (let-values ([(template rules) (file->inputs "test_input.txt")])
+    (format #t "~%Template: ~a" template)
+    (format #t "~%Rules: ~a" rules)))
