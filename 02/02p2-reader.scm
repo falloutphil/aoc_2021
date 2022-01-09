383fc1f22ef7b84c9c615248b1c9b67ad02fb84c
falloutphil <phil@beadling.co.uk>
2022-01-09 22:55:12 +0000

    first attempt at compiling the input with a modified reader

---
+++ 02/02p2-reader.scm
@@ -0,0 +1,46 @@
+#!/usr/bin/env -S guile -s
+!#
+
+(use-modules
+ (oop goops)
+ (system reader)
+ (system reader library)
+ (ice-9 textual-ports)
+ (ice-9 rdelim))
+
+(define hpos 0)
+(define vpos 0)
+(define aim 0)
+
+(define (forward n)
+  (set! hpos (+ hpos n))
+  (set! vpos (+ vpos (* aim n))))
+
+(define (up n)
+  (set! aim (- aim n)))
+
+(define (down n)
+  (set! aim (+ aim n)))
+
+(define (my-token-reader chr port reader top-level-reader)
+  (let ((fn-read (token-reader-procedure
+                  (standard-token-reader 'guile-symbol-lower-case)))
+	(ws-read (token-reader-procedure
+                  (standard-token-reader 'character)))
+	(num-read (token-reader-procedure
+                   (standard-token-reader 'character))))
+    (let* ((fn (fn-read chr port reader))
+	   (ws1 (ws-read chr port reader))
+	   (num (- (char->integer (num-read chr port reader)) 48)))
+      (list fn num))))
+
+(define my-reader
+  (make-reader (list (make-token-reader '(#\a . #\z) my-token-reader)
+		     (make-token-reader '(#\space #\newline #\tab) #f))))
+
+(load "input.txt" my-reader)
+(format #t "~%Result: ~a~%" (* hpos vpos))
+
+
+
+
