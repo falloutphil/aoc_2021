40125992bdf4cb4be3335cc224bd207194a49eff
falloutphil <phil@beadling.co.uk>
2022-01-15 11:52:08 +0000

    removing general whitespace reader to more it more explicit

--- 02/02p1-reader.scm
+++ 02/02p1-reader.scm
@@ -19,31 +19,21 @@
 (define (down n)
   (set! vpos (+ vpos n)))
 
-
-;; combine 2 readers into a list, rather than combine 2 token readers?
-
-;;(define (handle-fault chr port reader)
-;;  (format #t "~%FAULT: *~a*" chr)
-;;  " ")
-
-(define ignore-chars
-  (make-token-reader '(#\space #\newline #\tab) #f))
-
 (define my-fn-reader (make-reader (list (standard-token-reader 'guile-symbol-lower-case))))
-(define my-num-reader (make-reader (list ignore-chars
+(define my-num-reader (make-reader (list (make-token-reader #\space #f) ;; ignore space between func and number
 					 (standard-token-reader 'guile-number))))
 
 (define (my-token-reader-proc chr port reader top-level-reader)
-  (unget-char port chr)
+  (unget-char port chr) ;; can returning the character to the port?
+  ;; we use readers here not token-readers because readers
+  ;; move our position in the port along, TRs don't?
   (let* ((fn (my-fn-reader port))
 	 (num (my-num-reader port)))
-    (format #t "~%FN: *~a*, Num: *~a*" fn num)
     (list fn num)))
-
 
 (define my-reader
   (make-reader (list (make-token-reader '(#\a . #\z) my-token-reader-proc)
-		     ignore-chars)))
+		     (make-token-reader #\newline #f)))) ;; ignore newline immediately after parsing "func num"
 
 (load "input.txt" my-reader)
 (format #t "~%Result: ~a~%" (* hpos vpos))
