d187cc102e04406a59bf8d50372774b4955255bd
falloutphil <phil@beadling.co.uk>
2022-01-15 11:40:39 +0000

    improvements

--- 02/02p1-reader.scm
+++ 02/02p1-reader.scm
@@ -4,7 +4,8 @@
 ;; requires guile-reader >= 0.63
 
 (use-modules
- (system reader))
+ (system reader)
+ (ice-9 textual-ports))
 
 (define hpos 0)
 (define vpos 0)
@@ -18,24 +19,31 @@
 (define (down n)
   (set! vpos (+ vpos n)))
 
-(define fn-read (token-reader-procedure
-                 (standard-token-reader 'guile-symbol-lower-case)))
 
-(define ws-read (token-reader-procedure
-                 (standard-token-reader 'character))) ;; why not 'whitespace?
+;; combine 2 readers into a list, rather than combine 2 token readers?
 
-(define num-read (token-reader-procedure
-                  (standard-token-reader 'character))) ;; why not 'guile-number?
+;;(define (handle-fault chr port reader)
+;;  (format #t "~%FAULT: *~a*" chr)
+;;  " ")
 
-(define (my-token-reader chr port reader top-level-reader)
-  (let* ((fn (fn-read chr port reader))
-	 (_ (ws-read chr port reader)) ;; read and ignore whitespace character
-	 (num (- (char->integer (num-read chr port reader)) 48))) ;; crude char code -> number
+(define ignore-chars
+  (make-token-reader '(#\space #\newline #\tab) #f))
+
+(define my-fn-reader (make-reader (list (standard-token-reader 'guile-symbol-lower-case))))
+(define my-num-reader (make-reader (list ignore-chars
+					 (standard-token-reader 'guile-number))))
+
+(define (my-token-reader-proc chr port reader top-level-reader)
+  (unget-char port chr)
+  (let* ((fn (my-fn-reader port))
+	 (num (my-num-reader port)))
+    (format #t "~%FN: *~a*, Num: *~a*" fn num)
     (list fn num)))
 
+
 (define my-reader
-  (make-reader (list (make-token-reader '(#\a . #\z) my-token-reader)
-		     (make-token-reader '(#\space #\newline #\tab) #f))))
+  (make-reader (list (make-token-reader '(#\a . #\z) my-token-reader-proc)
+		     ignore-chars)))
 
 (load "input.txt" my-reader)
 (format #t "~%Result: ~a~%" (* hpos vpos))
